364d59493ccb0c8392635fe7b26e997eae22716c
Alexander <juanperzezss@gmail.com>
2025-04-24 14:59:06 -0500

    Methods have been added to the user service for login and registration

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,26 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>
+  ) { }
+
+  async create(uid, email) {
+    const user = this.userRepository.create({
+      firebaseUid: uid,
+      email: email,
+      personId: null,
+    })
+
+    return this.userRepository.save(user);
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findOneUid(uid: string) {
+    const user = await this.userRepository.findOne({ where: { firebaseUid: uid }, relations: ['personId'] });
+    if (!user) throw new NotFoundException('ERROR: user not Found');
+    return user;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
+    findAll() {
+      return `This action returns all user`;
+    }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+    findOne(id: number) {
+      return `This action returns a #${id} user`;
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+    update(id: number, updateUserDto: UpdateUserDto) {
+      return `This action updates a #${id} user`;
+    }
+
+    remove(id: number) {
+      return `This action removes a #${id} user`;
+    }
   }
-}
