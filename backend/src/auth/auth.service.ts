e43e6128edeb4053505308f1041be54d94ef2096
Alexander <juanperzezss@gmail.com>
2025-04-24 14:44:44 -0500

    the auth routes were created

---
+++ backend/src/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+  login() {
+    throw new Error('Method not implemented.');
+  }
+  register() {
+    throw new Error('Method not implemented.');
+  }
+}
