bb57f134d5d1041c9dadb56d2479b4bdf2d4b609
Alexander <juanperzezss@gmail.com>
2025-04-24 14:02:39 -0500

    Token validation method is added to the Firebase service

--- backend/src/firebase/firebase.service.ts
+++ backend/src/firebase/firebase.service.ts
@@ -9,12 +9,23 @@ export class FirebaseService {
     }
     private initializeFirebase() {
         if (!admin.apps.length) {
-            const serviceAccountData = serviceAccount as admin.ServiceAccount; 
+            const serviceAccountData = serviceAccount as admin.ServiceAccount;
             admin.initializeApp({
                 credential: admin.credential.cert(serviceAccountData),
             });
         }
     }
+    /**
+     * OTHER FUNCTIONS WITH THE FIREBASE CONSOLE SUCH AS TOKEN VALIDATION
+    */
 
-    
+    async verifyIdToken(idToken: string) {
+        try {
+            const decodedToken = await admin.auth().verifyIdToken(idToken);
+            return decodedToken;
+        } catch (error) {
+            throw new UnauthorizedException('Invalid token');
+        }
+    }
+
 }
